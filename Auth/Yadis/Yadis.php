bb5db98eaf1ef69a4c84f006a2f3258e5665f851
Will Norris <will@willnorris.com>
2008-02-11 23:01:18 +0000

    remove php5 dependency bug

--- Auth/Yadis/Yadis.php
+++ Auth/Yadis/Yadis.php
@@ -275,7 +275,7 @@ class Auth_Yadis_Yadis {
      * Auth_Yadis_Yadis, depending on whether the discovery
      * succeeded.
      */
-    function discover($uri, &$fetcher = null,
+    function discover($uri, &$fetcher,
                       $extra_ns_map = null, $timeout = 20)
     {
         $result = new Auth_Yadis_DiscoveryResult($uri);
