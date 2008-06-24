0ff25413579fda3926115634a4acb4c7b2550f75
Will Norris <will@willnorris.com>
2008-06-24 20:42:51 +0000

    cleanup whitespace (match JanRain library)

--- Auth/Yadis/Email.php
+++ Auth/Yadis/Email.php
@@ -37,19 +37,19 @@ define('Auth_Yadis_ETT_Wildcard_Username', '[username]');
  * Default service for email mapping.
  */
 if (!defined('Auth_Yadis_Default_Email_Mapper')) {
-	define('Auth_Yadis_Default_Email_Mapper', 'http://emailtoid.net/');
+    define('Auth_Yadis_Default_Email_Mapper', 'http://emailtoid.net/');
 }
 
 function Auth_Yadis_Email_getEmailTypeURIs() 
 {
-	return array(Auth_Yadis_ETT_Type,
-		         Auth_Yadis_EATOID_Type,
-				 Auth_Yadis_EmailToUrl_Type);
+    return array(Auth_Yadis_ETT_Type,
+                 Auth_Yadis_EATOID_Type,
+                 Auth_Yadis_EmailToUrl_Type);
 }
 
 function filter_MatchesAnyEmailType(&$service) 
 {
-	$uris = $service->getTypes();
+    $uris = $service->getTypes();
 
     foreach ($uris as $uri) {
         if (in_array($uri, Auth_Yadis_Email_getEmailTypeURIs())) {
@@ -65,66 +65,66 @@ function filter_MatchesAnyEmailType(&$service)
  * Function for performaing email addresss to ID translation.
  */
 function Auth_Yadis_Email_getID($email, $site_name = '') {
-	list($user, $domain) = split('@', $email, 2);
+    list($user, $domain) = split('@', $email, 2);
 
-	$fetcher = Auth_Yadis_Yadis::getHTTPFetcher();
+    $fetcher = Auth_Yadis_Yadis::getHTTPFetcher();
 
-	$services = Auth_Yadis_Email_getServices($domain, $fetcher);
-	if (empty($services)) {
-		$services = Auth_Yadis_Email_getServices(Auth_Yadis_Default_Email_Mapper, $fetcher);
-	}
+    $services = Auth_Yadis_Email_getServices($domain, $fetcher);
+    if (empty($services)) {
+        $services = Auth_Yadis_Email_getServices(Auth_Yadis_Default_Email_Mapper, $fetcher);
+    }
 
-	foreach ($services as $s) {
-		$types = $s->getTypes();
-		$uris = $s->getURIs();
+    foreach ($services as $s) {
+        $types = $s->getTypes();
+        $uris = $s->getURIs();
 
-		if (empty($types) || empty($uris)) {
-			continue;
-		}
+        if (empty($types) || empty($uris)) {
+            continue;
+        }
 
-		foreach ($types as $t) {
-			switch ($t) {
-				case Auth_Yadis_ETT_Type:
-					// TODO verify valid ETT
-					$id =  preg_replace('/'.preg_quote(Auth_Yadis_ETT_Wildcard_Username).'/', $user, $uris[0]);
-					break;
+        foreach ($types as $t) {
+            switch ($t) {
+                case Auth_Yadis_ETT_Type:
+                    // TODO verify valid ETT
+                    $id =  preg_replace('/'.preg_quote(Auth_Yadis_ETT_Wildcard_Username).'/', $user, $uris[0]);
+                    break;
 
-				case Auth_Yadis_EATOID_Type:
-				case Auth_Yadis_EmailToUrl_Type:
-					$url_parts = parse_url($uris[0]);
+                case Auth_Yadis_EATOID_Type:
+                case Auth_Yadis_EmailToUrl_Type:
+                    $url_parts = parse_url($uris[0]);
 
-					if (empty($url_parts['query'])) {
-						$id = $uris[0] . '?email=' . $email;
-					} else {
-						$id =  $uris[0] . '&email=' . $email;
-					}
-					
-					if ($t == Auth_Yadis_EmailToUrl_Type && $site_name) {
-						$id .= "&site_name=$site_name";
-					}
+                    if (empty($url_parts['query'])) {
+                        $id = $uris[0] . '?email=' . $email;
+                    } else {
+                        $id =  $uris[0] . '&email=' . $email;
+                    }
+                    
+                    if ($t == Auth_Yadis_EmailToUrl_Type && $site_name) {
+                        $id .= "&site_name=$site_name";
+                    }
 
-					break;
-			}
+                    break;
+            }
 
-			if ($id) {
-				return $id;
-			}
-		}
+            if ($id) {
+                return $id;
+            }
+        }
 
-	}
+    }
 
 }
 
 function Auth_Yadis_Email_getServices($uri, $fetcher) {
-	$uri = Auth_OpenID::normalizeUrl($uri);
+    $uri = Auth_OpenID::normalizeUrl($uri);
 
-	$response = Auth_Yadis_Yadis::discover($uri, $fetcher);
-	if ($response->isXRDS()) {
-		$xrds =& Auth_Yadis_XRDS::parseXRDS($response->response_text);
-		if ($xrds) {
-			return $xrds->services(array('filter_MatchesAnyEmailType'));
-		}
-	}
+    $response = Auth_Yadis_Yadis::discover($uri, $fetcher);
+    if ($response->isXRDS()) {
+        $xrds =& Auth_Yadis_XRDS::parseXRDS($response->response_text);
+        if ($xrds) {
+            return $xrds->services(array('filter_MatchesAnyEmailType'));
+        }
+    }
 }
 
 ?>
