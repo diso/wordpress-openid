565c94418ec7b1447208f55aa44db617cf9c006b
Will Norris <will@willnorris.com>
2008-10-20 23:54:34 +0000

    fix OpenID Server XRDS (pretty sure, this time)

--- server.php
+++ server.php
@@ -34,9 +34,9 @@ function openid_provider_xrds_simple($xrds) {
 	
 	if (!$user && get_option('openid_blog_owner')) {
 		$url_parts = parse_url(get_option('home'));
-		$script = '/' . preg_replace('/index.php$/', '', $url_parts['path']);
+		$script = preg_replace('/index.php$/', '', $_SERVER['SCRIPT_NAME']);
 
-		if ($script != $_SERVER['SCRIPT_NAME'] && !is_admin()) {
+		if ('/' . $url_parts['path'] != $script && !is_admin()) {
 			return $xrds;
 		}
 
