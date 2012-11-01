8d7c01f568b3d36baffc399829ac7b00627a8dd0
Rodrigo Primo <rodrigo@hacklab.com.br>
2012-11-01 16:17:34 -0200

    enqueue js instead of adding it directly to the html

---
+++ f/register.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+    jQuery(function() {
+        jQuery("#registerform > p:first").hide();
+        jQuery("#registerform > p:first + p").hide();
+        jQuery("#reg_passmail").hide();
+        jQuery("p.submit").css("margin", "1em 0");
+        var link = jQuery("#nav a:first");
+        jQuery("#nav").text("").append(link);
+    });
+});
